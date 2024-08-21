3315287fe90004560280165d0b58853ed491ea0e
stanislavmeshkov <stasmtschkoff@gmail.com>
2024-08-21 18:34:16 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,17 @@
+print("Hello world")
+
+'1st program'
+print((9 ** 0.5) * 5)
+
+"2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+"3rd program"
+print((2 * 2 + 2) == (2 * (2 + 2)))
+
+"4th program"
+'123.456'
+print(float('123.456'))
+print(123.456 * 10)
+print(int(1234.56))
+print(1234 % 10)
